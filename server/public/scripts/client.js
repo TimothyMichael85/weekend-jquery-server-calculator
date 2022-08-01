4e14acbf4cf8573693d5ceae5f398584ba903ec3
Tim Anderson <t.anderson0085@gmail.com>
2022-08-01 02:14:26 -0500

    server side rec.body

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,8 +6,19 @@ function onReady(){
 console.log('ready!');
 
 // add click listeners here
-$('#submitBtn').on('click', submitclicks);
+$('#submitBtn').on('click', submit);
 $('#clearBtn').on('click', clearClick);
 $('.numOperator').on('click', inputsToServer);
 
 }
+
+function submit(){
+    // collect inputs and append to DOM
+    // let operator = $('.numOperator');
+    const numInputs =  {
+        numOneInput: Number($('#numInputOne').val()),
+        numTwoInput: Number($('#numInputTwo').val()),
+        operator: numOperator
+    }
+}
+    console.log(numInputs);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 // imports
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 // server configuration
 const app = express();
@@ -10,8 +10,53 @@ const PORT = 5000;
 
 // Make static file
 app.use(express.static('server/Public'));
-//app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(PORT, function(){
     console.log('server listening on port', PORT)
 });
+
+app.post('/calc', (req, res) =>{
+    console.log('POST /calc', req.body);
+    // push to array
+    // calculations.push(req.body)
+    numInputs=req.body
+    // send back response
+    res.sendStatus(200);
+    myEquation();
+    })
+    app.get('/calc', (req,res) =>{
+        console.log('in get calc');
+        res.send(calculations);
+    })
+
+const calculations = [];
+
+let numInputs;
+let answer;
+
+function myEquation(){
+    switch (numInputs.operator) {
+        case '+':
+            answer = Number(numInputs.numOneInput) + Number(numInputs.numTwoInput)
+            break;
+            case '-':
+                answer = Number(numInputs.numOneInput) - Number(numInputs.numTwoInput)
+                break;
+                case '*':
+            answer = Number(numInputs.numOneInput) * Number(numInputs.numTwoInput)
+            break;
+            case '/':
+            answer = Number(numInputs.numOneInput) / Number(numInputs.numTwoInput)
+            break;
+    }
+
+let finalEval={
+    numOneInput: numInputs.numOneInput,
+    operator: numInputs.operator,
+    numTwoInput: numInputs.numTwoInput,
+    mathAnswer: answer
+}
+calculations.push(finalEval)
+    console.log(calculations)
+}
